4b41931cefbeec637074b0cd0ce7cdc92db55974
firefoxic <firefoxic.arts@gmail.com>
2018-06-09 22:56:06 +0300

    Add an output from the matrix along a spiral path

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const dimensionInput = document.querySelector('#dimension')
 const orderInput = document.querySelector('#order')
 const button = document.querySelector('#button')
 
+const spiralTemplate = (str) => html`
+  <p>${str}</p>
+`
+
 const getRandom = (maxOrder) => Math.floor(Math.random() * 10 ** maxOrder)
 
 const fillArray = (param, arrayLength, isMatrix) => {
@@ -18,11 +22,48 @@ const fillArray = (param, arrayLength, isMatrix) => {
   return result
 }
 
+const passSpirally = (matrix) => {
+  let row = (matrix.length - 1) / 2
+  let col = (matrix.length - 1) / 2
+  const result = []
+  const writeValue = () => {
+    result.push(matrix[row][col])
+  }
+  writeValue()
+  for (let i = 1; i <= matrix.length; i++) {
+    let steps = i
+    let isStepsOdd = steps % 2 !== 0
+    let step = isStepsOdd ? -1 : 1
+    const stepOnCol = () => {
+      for (let i = 0; i < steps; i++) {
+        col = col + step
+        writeValue()
+      }
+    }
+    const stepOnRow = () => {
+      for (let i = 0; i < steps; i++) {
+        row = row - step
+        writeValue()
+      }
+    }
+    if (steps === matrix.length) {
+      steps--
+      stepOnCol()
+      return result
+    }
+    stepOnCol()
+    stepOnRow()
+  }
+}
+
 const renderResult = () => {
   const numbersOrder = orderInput.value
   const matrixDimension = dimensionInput.value
   const matrixArray = fillArray(numbersOrder, matrixDimension, true)
+  const spiralArray = passSpirally(matrixArray)
+  const spiralString = spiralArray.join(', ')
   render(tableTemplate(matrixArray), matrixContainer)
+  render(spiralTemplate(spiralString), spiralContainer)
 }
 
 renderResult()
